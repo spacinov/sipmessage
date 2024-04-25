2141ebbeb0764084b3b3702c96e04821fde708a3
Jeremy Lainé <jeremy.laine@m4x.org>
2024-04-25 08:52:37 +0200

    Add URI and Parameters classes

---
+++ src/sipmessage/uri.py
@@ -0,0 +1,63 @@
+#
+# Copyright (C) Spacinov SAS
+# Distributed under the 2-clause BSD license
+#
+
+import dataclasses
+import typing
+import urllib.parse
+
+from .parameters import Parameters
+
+
+@dataclasses.dataclass
+class URI:
+    scheme: str
+    host: str
+    user: typing.Optional[str] = None
+    password: typing.Optional[str] = None
+    port: typing.Optional[int] = None
+    parameters: Parameters = dataclasses.field(default_factory=Parameters)
+
+    @classmethod
+    def parse(cls, value):
+        parsed = urllib.parse.urlparse(value)
+        if "@" in parsed.path:
+            user_password, host_port = parsed.path.split("@")
+            if ":" in user_password:
+                user, password = user_password.split(":")
+            else:
+                user = user_password
+                password = None
+        else:
+            host_port = parsed.path
+            user = None
+            password = None
+        if ":" in host_port:
+            host, port = host_port.split(":")
+            port = int(port)
+        else:
+            host = host_port
+            port = None
+        return cls(
+            scheme=parsed.scheme,
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            parameters=Parameters.parse(parsed.params),
+        )
+
+    def __str__(self):
+        s = self.scheme + ":"
+        if self.user is not None:
+            s += self.user
+            if self.password is not None:
+                s += ":" + self.password
+            s += "@"
+        s += self.host
+        if self.port is not None:
+            s += ":" + str(self.port)
+        if self.parameters:
+            s += ";" + str(self.parameters)
+        return s
